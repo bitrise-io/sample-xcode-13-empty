e0798822de2e18afd96ecf948cde066a1b6b5c4e
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2021-07-07 13:21:42 +0200

    iOS project init.

---
+++ sample-xcode-13-empty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sample-xcode-13-empty
+//
+//  Created by Krisztian Godrei on 2021. 07. 07..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sample-xcode-13-empty/sample_xcode_13_emptyApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_xcode_13_emptyApp.swift
+//  sample-xcode-13-empty
+//
+//  Created by Krisztian Godrei on 2021. 07. 07..
+//
+
+import SwiftUI
+
+@main
+struct sample_xcode_13_emptyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
